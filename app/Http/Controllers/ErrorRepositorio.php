704fd61fc5f9bf12dcfc39b5562ff962ae062191
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2020-12-09 14:16:39 -0300

    docs(ErrorRepositorio.php): Added some exceptions to list.

--- app/Http/Controllers/ErrorRepositorio.php
+++ app/Http/Controllers/ErrorRepositorio.php
@@ -9,9 +9,11 @@ class ErrorRepositorio
     private $SWERROR = array(
         "[SWERROR 001]: Problema al conectar con el servidor FTP.",
         "[SWERROR 002]: Problema al ingresar las credenciales de usuario FTP.",
-        "[SWERROR 003]: La empresa ya existe en el sistema (Conflicto en OperariosExternos).",
-        "[SWERROR 004]: La empresa ya existe en el sistema (Conflicto en OperariosInternos).",
-        "[SWERROR 005]: ???"
+        "[SWERROR 003]: La empresa ya existe en el sistema FTP (Conflicto en OperariosExternos).",
+        "[SWERROR 004]: La empresa ya existe en el sistema FTP (Conflicto en OperariosInternos).",
+        "[SWERROR 005]: La empresa no existe en el sistema FTP (Conflicto en OperariosExternos).",
+        "[SWERROR 006]: La empresa no existe en el sistema FTP (Conflicto en OperariosInternos).",
+        "[SWERROR 007]: ???",
     );
 
     // Dado un valor, retorna el error correspondiente.
