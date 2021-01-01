722f88a99445abfa28bd99daea1eceee836e365c
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-01 14:15:41 -0300

    feat(AndroidLogin.php): updated some parameters

--- app/Http/Controllers/AndroidAuth/AndroidLogin.php
+++ app/Http/Controllers/AndroidAuth/AndroidLogin.php
@@ -1,45 +1,61 @@
 <?php
 
- require("AndroidConexion.php");
+use Illuminate\Support\Facades\Hash;
 
-    // RECIBE LOS DATOS DE LA APP
-    $correo = $_POST['correo'];
-    $contraseniaOperario = $_POST['contraseniaOperario'];
+require("AndroidConexion.php");
 
-    // VERIFICAMOS QUE NO ESTEN VACIAS LAS VARIABLES
-    if(empty($correo) || empty($contraseniaOperario)) {
+// RECIBE LOS DATOS DE LA APP
+$rut = $_POST['rut'];
+$contraseniaOperario = $_POST['contraseniaOperario'];
 
-        // SI ALGUNA VARIABLE ESTA VACIA MUESTRA ERROR
-        //echo "Se deben llenar los dos campos";
-        echo "ERROR 1";
+// VERIFICAMOS QUE NO ESTEN VACIAS LAS VARIABLES
+if(empty($rut) || empty($contraseniaOperario)) {
 
-    } else {
+    // SI ALGUNA VARIABLE ESTA VACIA MUESTRA ERROR
+    //echo "Se deben llenar los dos campos";
+    echo "ERROR 1";
 
-        // CREAMOS LA CONSULTA
-        $sql = "SELECT * FROM operarios WHERE correo='$correo' AND contraseniaOperario='$contraseniaOperario'";
-        $query = $mysqli->query($sql);
+} else {
 
-        // CREAMOS UN ARRAY PARA GUARDAR LOS VALORES DEL REGISTRO
-        $data = array();
+    // CREAMOS LA CONSULTA
+    $sql = "SELECT operarios.nombre,operarios.rut,operarios.correo,operarios.contraseniaOperario,operarios.tipoOperario,operarios.contraseniaOperarioFTP,operarios.telefonoOperario,empresas.rut FROM operarios JOIN empresas ON empresas.id=operarios.empresa_id WHERE operarios.rut='$rut'";
+        
+        
+    $query = $mysqli->query($sql);
 
-        // VARIABLE CON EL TOTAL DE REGISTROS OBTENIDOS
-        $num = $query->num_rows;
+    // CREAMOS UN ARRAY PARA GUARDAR LOS VALORES DEL REGISTRO
+    $data = array();
 
-        //VERIFICAMOS QUE EXISTE ALGUN REGISTRO
-        if($num > 0) {
+    // VARIABLE CON EL TOTAL DE REGISTROS OBTENIDOS
+    $num = $query->num_rows;
+
+    //VERIFICAMOS QUE EXISTE ALGUN REGISTRO
+    if($num > 0) {
             
-            // AGREGAMOS LOS VALORES AL ARRAY
-            while($resultado = $query->fetch_assoc()) {
-                $data[] = $resultado;
+        $operarioEncontrado = false;
 
+        // AGREGAMOS LOS VALORES AL ARRAY
+        while($resultado = $query->fetch_assoc()) {
+
+            $data[] = $resultado;
+
+            if(password_verify($contraseniaOperario,$data[0]['contraseniaOperario']) && $operarioEncontrado == false){
+
+                $operarioEncontrado = true;
                 // CREAMOS EL JSON Y LO MOSTRAMOS
                 echo json_encode($data);
-            }
-
-        } else {
-            // echo "No existe ese registro";
-            echo "ERROR 2";
+            }                
         }
-    }
 
+        if($operarioEncontrado == false){ 
+            
+            // echo usuario o contraseña no validos";
+            echo "ERROR 3";
+        }
+
+    } else {
+        // echo "No existe ese registro";
+        echo "ERROR 2";
+    }
+}
 ?>
