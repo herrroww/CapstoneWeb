1701798dc0036e130d7a5437b2d1652a7d349c50
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-01 17:21:37 -0300

    feat(EmpresaController.php): updated FTP CRUD functionalities

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Empresa;
+use App\Operario;
 use App\User;
 use App\AuditTrail;
 use Illuminate\Support\Facades\Auth;
@@ -14,8 +15,7 @@ use App\Http\Controllers\ErrorRepositorio;
 
 use phpseclib\Net\SSH2;
 
-class EmpresaController extends Controller
-{  
+class EmpresaController extends Controller{  
 
     //IP del servidor FTP.
     private $serverFTP = '192.168.0.28';
@@ -25,14 +25,15 @@ class EmpresaController extends Controller
     private $passFTP= 'capstone';
 
 
-    public function __construct()
-    {
+    public function __construct(){
+
         $this->middleware('auth');
     }
 
     public function index(Request $request){
 
         if($request){
+
             $query = trim($request->get('search'));
 
             $empresas = Empresa::where('nombre',  'LIKE', '%' . $query . '%')
@@ -43,28 +44,11 @@ class EmpresaController extends Controller
 
             return view('empresas.index', ['empresas' => $empresas, 'search' => $query, 'activemenu' => 'empresa']);
         } 
-        
     }
     
     public function create(Request $request){
         
-
-        /*$user = Auth::user();
-        $data = $request->validate(
-            [
-                'rut' => 'required',
-                'nombre' => 'required',
-                'compañia' => 'required',
-            ]
-            );
-        DB::beginTransaction();
-            $empresa_data = EmpresaData::query()->create($data);
-            $user->log("CREATED DATA {$empresa_data->nombre}");
-            
-        DB::commit();*/
-
-        return view('empresas.create',['activemenu' => 'empresa']);       
-        
+        return view('empresas.create',['activemenu' => 'empresa']);
     }
 
     public function store(Request $request){
@@ -86,50 +70,62 @@ class EmpresaController extends Controller
         //Intenta hacer la conexion al servidor FTP.
         if(!$ssh->login($this->userFTP,$this->passFTP)){
             
+            //TODO: Actualizar sistema de errores.
+            //Se liberan los recursos.       
+            unset($SWERROR);
+            unset($ssh);
             //[SWERROR 002]: Problema al ingresar las credenciales de usuario FTP.
             exit($SWERROR->ErrorActual(1));
+
         }else{
 
             //Verifica si el directorio existe.
-            $estadoExiste = $ssh->exec('[ -d /home/capstone/ftp/OperariosExternos/'.$empresa->rut.' ] && echo "true" || echo "false"');
+            $estadoExiste = $ssh->exec('[ -d /home/Externo/'.$empresa->rut.' ] && echo "1" || echo "0"');
             
             //Limpia la informacion obtenida.
-            $estadoExiste = $estadoExiste[0].$estadoExiste[1].$estadoExiste[2].$estadoExiste[3];
+            $estadoExiste = $estadoExiste[0];
             
-            if($estadoExiste == 'true'){
+            if($estadoExiste == '1'){
 
+                //TODO: Actualizar sistema de errores.
+                //Se liberan los recursos.       
+                unset($SWERROR);
+                unset($ssh);
                 //[SWERROR 003]: La empresa ya existe en el sistema (Conflicto en OperariosExternos).
                 exit($SWERROR->ErrorActual(2));
             }else{
 
                 //Verifica si el directorio existe.
-                $estadoExiste = $ssh->exec('[ -d /home/capstone/ftp/OperariosInternos/'.$empresa->rut.' ] && echo "true" || echo "false"');
+                $estadoExiste = $ssh->exec('[ -d /home/Interno/'.$empresa->rut.' ] && echo "1" || echo "0"');
                 
                 //Limpia la informacion obtenida.
-                $estadoExiste = $estadoExiste[0].$estadoExiste[1].$estadoExiste[2].$estadoExiste[3];
+                $estadoExiste = $estadoExiste[0];
 
-                if($estadoExiste == 'true'){
+                if($estadoExiste == '1'){
 
+                    //TODO: Actualizar sistema de errores.
+                    //Se liberan los recursos.       
+                    unset($SWERROR);
+                    unset($ssh);
                     //[SWERROR 004]: La empresa ya existe en el sistema (Conflicto en OperariosInternos).
                     exit($SWERROR->ErrorActual(3));
+
                 }else{
 
                     //Se crea el directorio de la empresa.
-                    $ssh->exec('mkdir /home/capstone/ftp/OperariosExternos/'.$empresa->rut);
-                    $ssh->exec('mkdir /home/capstone/ftp/OperariosInternos/'.$empresa->rut);
+                    $ssh->exec('echo '.$this->passFTP.' | sudo -S mkdir /home/Externo/'.$empresa->rut);
+                    $ssh->exec('echo '.$this->passFTP.' | sudo -S mkdir /home/Interno/'.$empresa->rut);
 
                     //Se almacena la empresa en la base de datos.
                     $empresa->save();
+
+                    //Se liberan los recursos.       
+                    unset($SWERROR);
+                    unset($ssh);
                 }
             }
         } 
-            
-        //Se liberan los recursos.       
-        unset($SWERROR);
-        unset($ssh);
-
         return redirect('empresaop')->with('create','La empresa se a creado correctamente');
-
     }
 
     public function edit($id){
@@ -141,12 +137,11 @@ class EmpresaController extends Controller
 
         //Carga el repositorio de errores.
         $SWERROR = new ErrorRepositorio();
-        $empresa = Empresa::findOrFail($id);
-
+        
         $user = Auth::user();
         
         //Busca a la empresa dada una id de la tabla.
-        $empresa = Empresa::findOrFail($id);
+        $empresa = Empresa::findOrFail($id);        
 
         //Se extrae el rut antiguo de la empresa.
         $rutTemp = $empresa->rut;
@@ -156,53 +151,92 @@ class EmpresaController extends Controller
         $empresa->nombre = $request->get('nombre');
         $empresa->compania = $request->get('compania');
         
-        //Se prepara la conexion al servidor FTP.
-        $ssh = new SSH2($this->serverFTP);
+        //Verifica si el nuevo rut de la empresa es diferente al antiguo.
+        if($rutTemp != $empresa->rut){
+
+            //TODO: Actualizar sistema de errores.
+            //Se liberan los recursos.       
+            unset($SWERROR);
+            unset($ssh);
+            //Prepara la conexion al servidor FTP.
+            $ssh = new SSH2($this->serverFTP);
               
-        //Intenta hacer la conexion al servidor FTP.
-        if(!$ssh->login($this->userFTP,$this->passFTP)){
+            //Intenta hacer la conexion al servidor FTP.
+            if(!$ssh->login($this->userFTP,$this->passFTP)){
             
-            //[SWERROR 002]: Problema al ingresar las credenciales de usuario FTP.
-            exit($SWERROR->ErrorActual(1));
-        }else{
-
-            //Verifica si el directorio existe.
-            $estadoExiste = $ssh->exec('[ -d /home/capstone/ftp/OperariosExternos/'.$rutTemp.' ] && echo "true" || echo "false"');
-            
-            //Limpia la informacion obtenida.
-            $estadoExiste = $estadoExiste[0].$estadoExiste[1].$estadoExiste[2].$estadoExiste[3];
-            
-            if($estadoExiste != 'true'){
-
-                //[SWERROR 005]: La empresa no existe en el sistema FTP (Conflicto en OperariosExternos).
-                exit($SWERROR->ErrorActual(4));
+                //TODO: Actualizar sistema de errores.
+                //Se liberan los recursos.       
+                unset($SWERROR);
+                unset($ssh);
+                //[SWERROR 002]: Problema al ingresar las credenciales de usuario FTP.
+                exit($SWERROR->ErrorActual(1));
             }else{
 
-                //Verifica si el directorio existe.
-                $estadoExiste = $ssh->exec('[ -d /home/capstone/ftp/OperariosInternos/'.$rutTemp.' ] && echo "true" || echo "false"');
-                
+                //Verifica si el directorio existe en el directorio Externo.
+                $estadoExiste = $ssh->exec('[ -d /home/Externo/'.$rutTemp.' ] && echo "1" || echo "0"');
+            
                 //Limpia la informacion obtenida.
-                $estadoExiste = $estadoExiste[0].$estadoExiste[1].$estadoExiste[2].$estadoExiste[3];
+                $estadoExiste = $estadoExiste[0];
+            
+                if($estadoExiste != '1'){
 
-                if($estadoExiste != 'true'){
-
-                    //[SWERROR 006]: La empresa no existe en el sistema FTP (Conflicto en OperariosInternos).
-                    exit($SWERROR->ErrorActual(5));
+                    //TODO: Actualizar sistema de errores.
+                    //Se liberan los recursos.       
+                    unset($SWERROR);
+                    unset($ssh);
+                    //[SWERROR 005]: La empresa no existe en el sistema FTP (Conflicto en directorio Externo).
+                    exit($SWERROR->ErrorActual(4));
                 }else{
 
-                    //Se cambia el nombre del directorio de la empresa.
-                    $ssh->exec('mv /home/capstone/ftp/OperariosExternos/'.$rutTemp.' /home/capstone/ftp/OperariosExternos/'.$empresa->rut);
-                    $ssh->exec('mv /home/capstone/ftp/OperariosInternos/'.$rutTemp.' /home/capstone/ftp/OperariosInternos/'.$empresa->rut);
-                    
-                    //Se actualizan los cambios en la base de datos.
-                    $empresa->update();
+                    //Verifica si el directorio existe en el directorio Interno.
+                    $estadoExiste = $ssh->exec('[ -d /home/Interno/'.$rutTemp.' ] && echo "1" || echo "0"');
+                
+                    //Limpia la informacion obtenida.
+                    $estadoExiste = $estadoExiste[0];
+
+                    if($estadoExiste != '1'){
+
+                        //TODO: Actualizar sistema de errores.
+                        //Se liberan los recursos.       
+                        unset($SWERROR);
+                        unset($ssh);
+                        //[SWERROR 006]: La empresa no existe en el sistema FTP (Conflicto en directorio Interno).
+                        exit($SWERROR->ErrorActual(5));
+                    }else{
+
+                        //Cambia el nombre del directorio de la empresa.
+                        $ssh->exec('echo '.$this->passFTP.' | sudo -S mv /home/Externo/'.$rutTemp.' /home/Externo/'.$empresa->rut);
+                        $ssh->exec('echo '.$this->passFTP.' | sudo -S mv /home/Interno/'.$rutTemp.' /home/Interno/'.$empresa->rut);
+
+                        //Se obtienen todos los operarios vinculados la empresa.
+                        $operarios = DB::table('operarios')
+                            ->join('empresas', 'operarios.empresa_id', '=', 'empresas.id')
+                            ->select('operarios.*')
+                            ->get();
+
+                        //Asigna nuevo home a los Operarios relacionados con la empresa.
+                        foreach($operarios as $operario){
+
+                            //El Operario es Interno, se le reasigna el home.
+                            if($operario->tipoOperario=="Interno"){
+
+                                $ssh->exec('echo '.$this->passFTP." | sudo -S usermod -d /home/Interno/".$empresa->rut." ".$operario->rut);
+                            }else{
+
+                                //En cualquier otro caso, se establece Operario Externo por defecto.
+                                $ssh->exec('echo '.$this->passFTP." | sudo -S usermod -d /home/Externo/".$empresa->rut."/".$operario->rut." ".$operario->rut);
+                            }                      
+                        }
+
+                        //Se actualizan los cambios en la base de datos.
+                        $empresa->update();
+                        //Se liberan los recursos.       
+                        unset($SWERROR);
+                        unset($ssh);
+                    }
                 }
             }
-        } 
-            
-        //Se liberan los recursos.       
-        unset($SWERROR);
-        unset($ssh);        
+        }    
 
         return redirect('empresaop')->with('edit','La empresa se a editado');
     }
@@ -219,54 +253,78 @@ class EmpresaController extends Controller
               
         //Intenta hacer la conexion al servidor FTP.
         if(!$ssh->login($this->userFTP,$this->passFTP)){
-            
+
+            //TODO: Actualizar sistema de errores.
+            //Se liberan los recursos.       
+            unset($SWERROR);
+            unset($ssh);            
             //[SWERROR 002]: Problema al ingresar las credenciales de usuario FTP.
             exit($SWERROR->ErrorActual(1));
         }else{
 
             //Verifica si el directorio existe.
-            $estadoExiste = $ssh->exec('[ -d /home/capstone/ftp/OperariosExternos/'.$empresa->rut.' ] && echo "true" || echo "false"');
+            $estadoExiste = $ssh->exec('[ -d /home/Externo/'.$empresa->rut.' ] && echo "1" || echo "0"');
             
             //Limpia la informacion obtenida.
-            $estadoExiste = $estadoExiste[0].$estadoExiste[1].$estadoExiste[2].$estadoExiste[3];
+            $estadoExiste = $estadoExiste[0];
             
-            if($estadoExiste != 'true'){
+            if($estadoExiste != '1'){
 
+                //TODO: Actualizar sistema de errores.
+                //Se liberan los recursos.       
+                unset($SWERROR);
+                unset($ssh);
                 //[SWERROR 005]: La empresa no existe en el sistema FTP (Conflicto en OperariosExternos).
                 exit($SWERROR->ErrorActual(4));
             }else{
 
                 //Verifica si el directorio existe.
-                $estadoExiste = $ssh->exec('[ -d /home/capstone/ftp/OperariosInternos/'.$empresa->rut.' ] && echo "true" || echo "false"');
+                $estadoExiste = $ssh->exec('[ -d /home/Interno/'.$empresa->rut.' ] && echo "1" || echo "0"');
                 
                 //Limpia la informacion obtenida.
-                $estadoExiste = $estadoExiste[0].$estadoExiste[1].$estadoExiste[2].$estadoExiste[3];
+                $estadoExiste = $estadoExiste[0];
 
-                if($estadoExiste != 'true'){
+                if($estadoExiste != '1'){
 
+                    //Se liberan los recursos.       
+                    unset($SWERROR);
+                    unset($ssh);
                     //[SWERROR 006]: La empresa no existe en el sistema FTP (Conflicto en OperariosInternos).
                     exit($SWERROR->ErrorActual(5));
                 }else{
 
                     //Se elimina el directorio de la empresa.
-                    $ssh->exec('rm -r /home/capstone/ftp/OperariosExternos/'.$empresa->rut);
-                    $ssh->exec('rm -r /home/capstone/ftp/OperariosInternos/'.$empresa->rut);
+                    $ssh->exec('echo '.$this->passFTP.' | sudo -S rm -r /home/Externo/'.$empresa->rut);
+                    $ssh->exec('echo '.$this->passFTP.' | sudo -S rm -r /home/Interno/'.$empresa->rut);
 
                     //Se envia el directorio de la empresa a la basura. (Version Opcional)
-                    //$ssh->exec('gvfs-trash /home/capstone/ftp/OperariosExternos/'.$empresa->rut);
-                    //$ssh->exec('gvfs-trash /home/capstone/ftp/OperariosInternos/'.$empresa->rut);
-                    
+                    //$ssh->exec('echo '.$this->passFTP.' | sudo -S gvfs-trash /home/Externo/'.$empresa->rut);
+                    //$ssh->exec('echo '.$this->passFTP.' | sudo -S gvfs-trash /home/Interno/'.$empresa->rut);
+
+                    //Se obtienen todos los operarios vinculados la empresa.
+                    $operarios = DB::table('operarios')
+                        ->join('empresas', 'operarios.empresa_id', '=', 'empresas.id')
+                        ->select('operarios.*')
+                        ->get();
+
+                    //Elimina las cuentas de Operarios relacionadas con la empresa.
+                    foreach($operarios as $operario){
+
+                        $ssh->exec('echo '.$this->passFTP.' | sudo -S userdel '.$operario->rut);
+                    }
+
+                    $ssh->exec('exit');
+
                     //Se elimina la empresa de la base de datos.
                     $empresa->delete();
+                       
+                    //Se liberan los recursos.       
+                    unset($SWERROR);
+                    unset($ssh);
                 }
             }
-        } 
-            
-        //Se liberan los recursos.       
-        unset($SWERROR);
-        unset($ssh);
+        }  
         
-        return redirect()->back()->with('success','La empresa a sido eliminada.'); 
-        
+        return redirect()->back()->with('success','La empresa a sido eliminada.');         
     }
 }
