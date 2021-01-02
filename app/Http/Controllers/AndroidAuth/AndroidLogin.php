66767124d0790eae870b48ddabbf1dbbccb78c91
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-02 13:30:16 -0300

    feat(AndroidLogin.php): added some functionalities

--- app/Http/Controllers/AndroidAuth/AndroidLogin.php
+++ app/Http/Controllers/AndroidAuth/AndroidLogin.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Hash;
 require("AndroidConexion.php");
 
 // RECIBE LOS DATOS DE LA APP
-$rut = $_POST['rut'];
+$rut = $_POST['rutOperario'];
 $contraseniaOperario = $_POST['contraseniaOperario'];
 
 // VERIFICAMOS QUE NO ESTEN VACIAS LAS VARIABLES
@@ -19,7 +19,7 @@ if(empty($rut) || empty($contraseniaOperario)) {
 
     // CREAMOS LA CONSULTA
     $sql = "SELECT operarios.nombre,operarios.rut,operarios.correo,operarios.contraseniaOperario,operarios.tipoOperario,operarios.contraseniaOperarioFTP,operarios.telefonoOperario,empresas.rut FROM operarios JOIN empresas ON empresas.id=operarios.empresa_id WHERE operarios.rut='$rut'";
-        
+    
     $query = $mysqli->query($sql);
 
     // CREAMOS UN ARRAY PARA GUARDAR LOS VALORES DEL REGISTRO
@@ -31,7 +31,7 @@ if(empty($rut) || empty($contraseniaOperario)) {
     //VERIFICAMOS QUE EXISTE ALGUN REGISTRO
     if($num > 0) {
             
-        $operarioEncontrado = false;
+        $operarioEncontrado = false;        
 
         // AGREGAMOS LOS VALORES AL ARRAY
         while($resultado = $query->fetch_assoc()) {
@@ -41,6 +41,15 @@ if(empty($rut) || empty($contraseniaOperario)) {
             if(password_verify($contraseniaOperario,$data[0]['contraseniaOperario']) && $operarioEncontrado == false){
 
                 $operarioEncontrado = true;
+
+                //Se extrae la informacion obtenida.
+                $rutOperario = $data[0]['rut'];
+                $nombreOperario = $data[0]['nombre'];
+
+                //Se inserta en la base de datos de registro de sesion.
+                $sqlControlSesion = "INSERT INTO registrarsesionoperarios (rutOperario,nombreOperario,mensajeEvento,created_at) VALUES ('$rutOperario','$nombreOperario','Ha iniciado sesion',now())";
+                $mysqli->query($sqlControlSesion);
+
                 // CREAMOS EL JSON Y LO MOSTRAMOS
                 echo json_encode($data);
             }                
