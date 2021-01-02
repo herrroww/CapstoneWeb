ddd60ef64da069460319b2cd22578cf886f99167
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-02 13:30:58 -0300

    feat(registrarsesionoperarios.table): edited some columns

--- database/migrations/2020_12_16_214243_create_registrarsesionoperarios_table.php
+++ database/migrations/2020_12_16_214243_create_registrarsesionoperarios_table.php
@@ -15,10 +15,9 @@ class CreateRegistrarsesionoperariosTable extends Migration
     {
         Schema::create('registrarsesionoperarios', function (Blueprint $table) {
             $table->id();
-            $table->string('rutOperario', 100)->unique();
+            $table->string('rutOperario', 100);
             $table->string('nombreOperario', 100);
             $table->string('mensajeEvento', 100);
-            $table->string('fechaEvento', 100);
 
             $table->timestamps();
         });
