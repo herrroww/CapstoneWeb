0a5974b6cd3146bf2032514386784790bb9dd599
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-02 22:30:51 -0300

    chore(FtpConexion.php): added ftp connection class

---
+++ app/Http/Controllers/FtpConexion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// Clase encargada de almacenar repositorio de los errores relacionados al sistema web.
+class FtpConexion{   
+
+    //IP del servidor FTP.
+    private $serverFTP = '192.168.0.28';
+    
+    //Credenciales de usuario FTP
+    private $userFTP= 'capstone';
+    private $passFTP= 'capstone';
+
+    //Retorna la IP del servidor FTP.
+    public function getServerFTP(){
+        return $this->serverFTP;
+    }
+
+    //Retorna el nombre de usuario FTP.
+    public function getUserFTP(){
+        return $this->userFTP;
+    }
+
+    //Retorna la contraseña de usuario FTP.
+    public function getPassFTP(){
+        return $this->passFTP;
+    }
+}
