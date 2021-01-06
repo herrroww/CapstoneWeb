c99b5cf10b9ef97a648084d8a866cb060d580d0d
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-06 11:32:20 -0300

    feat(AndroidReport.php): added new POST parameter

--- app/Http/Controllers/AndroidAuth/AndroidReport.php
+++ app/Http/Controllers/AndroidAuth/AndroidReport.php
@@ -15,26 +15,27 @@ $prioridad = $_POST['prioridad'];
 $estado = $_POST['estado'];
 $fechaReporteProblema = $_POST['fechaReporteProblema'];
 $tituloReporteProblema = $_POST['tituloReporteProblema'];
+$codigoComponente = $_POST['codigoComponente'];
 $contenidoReporteProblema = $_POST['contenidoReporteProblema'];
 
 $consulta = "INSERT INTO reporteproblemas (rutOperario,nombreOperario,correoOperario,numeroOperario,prioridad,estado,fechaReporteProblema,tituloReporteProblema,contenidoReporteProblema,created_at) VALUES ('$rutOperario','$nombreOperario','$correoOperario','$numeroOperario','$prioridad','$estado','$fechaReporteProblema','$tituloReporteProblema','$contenidoReporteProblema',now())";
 
 
 // VERIFICAMOS QUE NO ESTEN VACIAS LAS VARIABLES
-if(empty($rutOperario) || empty($nombreOperario) || empty($correoOperario) || empty($numeroOperario) || empty($prioridad) || empty($estado) || empty($fechaReporteProblema) || empty($tituloReporteProblema) || empty($contenidoReporteProblema)) {
+if(empty($rutOperario) || empty($nombreOperario) || empty($correoOperario) || empty($numeroOperario) || empty($prioridad) || empty($estado) || empty($fechaReporteProblema) || empty($tituloReporteProblema) || empty($codigoComponente) || empty($contenidoReporteProblema)) {
 
     // SI ALGUNA VARIABLE ESTA VACIA MUESTRA ERROR
     //echo "Se deben llenar los dos campos";
-    echo "ERROR 1";
+    die("ERROR 1");
 
 } else {
    
     if($mysqli->query($consulta)){
 
-        echo "Recibido";
+        die("Recibido");
     }else{
 
-        echo "Rechazado";
+        die("Rechazado");
     }
 }
 ?>
