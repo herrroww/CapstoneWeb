821c8af3b05fed83ce05ba2317081171f06e8440
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-10 16:37:35 -0300

    feat(Componente.php): added history management

--- app/Componente.php
+++ app/Componente.php
@@ -8,16 +8,10 @@ use Illuminate\Notifications\Notifiable;
 use App\Modelo;
 use App\Documento;
 use App\Componente;
-use OwenIt\Auditing\Contracts\Auditable;
 
 
-class Componente extends Model implements Auditable{
+class Componente extends Model{
 
-    use \OwenIt\Auditing\Auditable;
-    
-    /*public function modelo(){
-        return $this->hasMany("App\Modelo");
-    }*/
     public function documento(){
 
         return $this->hasMany("App\Documento");
