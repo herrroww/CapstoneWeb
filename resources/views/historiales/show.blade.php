58dca125f40343a1c7415b462029b979213dd040
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-11 01:08:41 -0300

    feat(historiales.show): changed table form

--- resources/views/historiales/show.blade.php
+++ resources/views/historiales/show.blade.php
@@ -2,100 +2,68 @@
 
 @section('content')
 
-<div class="container">
+  <div class="container">
     <div class="col-12 pt-3 pb-3 text-center" >
-        <h2>Cambios Realizados</h2>
+      <h2>Cambios Realizados</h2>
     </div>
-    <hr>  
-    
-<div class="container">
-        <table class="table table-bordered " >
-          <thead >
-            <tr>
-            @if($audit->old_values!=null && $audit->new_values==null)
-             <th scope="col" class=" text-center bg-blue color-white">Valor Borrado</th>
-              
-              @elseif($audit->old_values==null && $audit->new_values!=null)
-              <th scope="col" class=" text-center bg-blue color-white">Valor Agregado</th>
-              
-              @else
-              <th scope="col" class="text-center bg-blue color-white">Valor Borrado</th>
-              <th scope="col" class="text-center bg-blue color-white">Valor Agregado</th>
-              @endif
-            </tr>
-          </thead>
+    <hr>    
+  <div class="container">
+    <table class="table table-bordered " >
+      <thead >
+        <tr>            
+          <th scope="col" class="text-center bg-blue color-white">Contenido</th>
+        </tr>
+      </thead>
           
-          <tbody id="audits" >
-            <tr>
-                <td>
-                <table class="table-bordered ">
-                
-                @if($audit->old_values!=null && $audit->new_values==null)
-                </table>
-                <table class="table ">
-                    @foreach($audit->old_values as $attribute => $value)
-                    @if($attribute!="contraseniaOperario" && $attribute!="contraseniaOperarioFTP" && $value!="" && $attribute!="")
-                      <tr>
-                        <td><b>{{ $attribute }}</b></td>
-                        <td>{{ $value }}</td>
-                      </tr>
-                    @endif
-                    @endforeach             
-                </td>
+      <tbody id="historicogestion" >
+        <tr>
+          <td>
+            <table class="table-bordered "></table>
 
-                
-                <td>
-                @elseif($audit->old_values==null && $audit->new_values!=null)
-                </table>
-                <table class="table ">
-                  
-                    @foreach($audit->new_values as $attribute => $value)
-                    @if($attribute!="contraseniaOperario" && $attribute!="contraseniaOperarioFTP" && $value!=""  && $attribute!="")
-                      <tr>
-                        <td><b>{{ $attribute }}</b></td>
-                        <td>{{ $value }}</td>
-                      </tr>
-                      @endif
-                    @endforeach
-                  
-                </td>
-                @else
-                  <table class="table">
-                    @foreach($audit->old_values as $attribute => $value)
-                    @if($attribute!="contraseniaOperario" && $attribute!="contraseniaOperarioFTP" && $value!="")
-                      <tr>
-                        <td><b>{{ $attribute }}</b></td>
-                        <td>{{ $value }}</td>
-                      </tr>
-                      @endif
-                    @endforeach
-                  </table>
-                </td>
-                <td>
-                  <table class="table">
-                    @foreach($audit->new_values as $attribute => $value)
-                    @if($attribute!="contraseniaOperario" && $attribute!="contraseniaOperarioFTP" && $value!="")
-                      <tr>
-                        <td><b>{{ $attribute }}</b></td>
-                        <td>{{ $value }}</td>
-                      </tr>
-                      @endif
-                    @endforeach
-                  </table>
-                </td>             
-              </tr>           
-          </tbody>
-          @endif
-        </table>
-    
-          <a href="{{ url()->previous() }}">
-  <div style="position: absolute; left: 90%; bottom: 10%;">
-  <button type="button" class="btn btn-secondary">Volver</button>
- </div>
-</a>
+            <table class="table table-bordered">
+              <thead></thead></td>
+        </tr>
+      </tbody>
 
-</table>
-       
+        <tr>
+          <th scope="row">ID:</th>
+          <td>{{$historicogestion->id}}</td>
+        </tr>
 
-      </div>
-    @endsection
+        <tr>
+          <th scope="row">Gestión: </th>
+          <td>{{$historicogestion->nombreGestion}}</td>
+        </tr>
+
+        <tr>
+          <th scope="row">Acción: </th>
+          <td>{{$historicogestion->tipoGestion}}</td>
+        </tr>
+
+        <tr>
+          <th scope="row">Responsable: </th>
+          <td>{{$historicogestion->responsableGestion}}</td>
+        </tr>
+
+        <tr>
+          <th scope="row">Descripción: </th>
+          <td>{{$historicogestion->descripcionGestion}}</td>
+        </tr>
+        
+        
+         
+    </table>
+
+    <form>
+    <a href="{{ route('reporteop') }}">
+    <button style="margin-top: 20px" type="button" class="btn btn-secondary float-right">Volver</button>
+    </a>
+    </form>      
+    </table>  
+        
+         
+
+  </div>
+   
+</div>     
+@endsection
