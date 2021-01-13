9993688690d495903cb432435edeaae71c05f07f
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-13 13:26:40 -0300

    feat(empresas.create): updated validation

--- resources/views/empresas/create.blade.php
+++ resources/views/empresas/create.blade.php
@@ -12,24 +12,24 @@
     @csrf
   <div class="form-group">
     <label for="nombreEmpresa">Nombre:</label>
-    <input type="text" class="form-control" name="nombreEmpresa" placeholder="Escriba nombre empresa" required>
+    <input type="text" class="form-control" name="nombreEmpresa" placeholder="Escriba nombre empresa" value="{{ old('nombreEmpresa') }}" required>
+    {!! $errors->first('nombreEmpresa','<div class="alert alert-danger"><small>:message</small></div><br>') !!}
   </div>
 
   <form>
   <div class="form-group">
   <label style="margin-top: 20px" for="rutEmpresa">Rut: </label><div style="margin-right: 100px" class="alert alert-info float-right" role="alert">
   Colocar rut con puntos y guión ejemplo: 11.111.111-1</div>
-    <input type="text" class="form-control" name="rutEmpresa" placeholder="Escriba rut de empresa" required>
+    <input type="text" class="form-control" name="rutEmpresa" placeholder="Escriba rut de empresa" value="{{ old('nombreEmpresa') }}" required>
+    {!! $errors->first('rutEmpresa','<div class="alert alert-danger"><small>:message</small></div><br>') !!}
   </div>
 
   <form>
   <div class="form-group">
     <label for="compania">Compañía:</label>
-    <input type="text" class="form-control" name="compania" placeholder="Escriba la compañía" required>
+    <input type="text" class="form-control" name="compania" placeholder="Escriba la compañía" value="{{ old('compania') }}" required>
+    {!! $errors->first('compania','<div class="alert alert-danger"><small>:message</small></div><br>') !!}
   </div>
-
-  
-
   
   <button style="margin-top: 20px" type="submit" class="btn btn-primary ">Enviar</button>
   
