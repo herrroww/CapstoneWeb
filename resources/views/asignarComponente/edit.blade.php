2ed4df490982d8bdcbe0b59e2043234683e4d714
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-16 21:24:04 -0300

    feat(asignarComponente): changed validation view

--- resources/views/asignarComponente/edit.blade.php
+++ resources/views/asignarComponente/edit.blade.php
@@ -7,15 +7,6 @@
         <div class="col-sm-6">
             <h3>Editar</h3>
             <hr>
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-                 </ul>
-                    </div>
-                    @endif
 
 <form action="" method="POST">
     @method('PATCH')
@@ -25,16 +16,12 @@
  <strong style="margin-bot: 10px">Operario:</strong>
  <select name="operario" class="form-control" id="operariosselect">
 
- @if($asignar->operario == null)
- <option value="">No hay operario seleccionada</option>
- @else
- <option value="{{ $asignar->operario_id }}" required>{{ $asignar->operario->nombreOperario}}</option>
- @endif
  
- @foreach($operario as $operarios)
-@if($operarios->id != $asignar->operario_id)
-  <option value="{{ $operarios->id }}">{{ $operarios->nombreOperario }}</option>
-  @endif
+ <option value="{{ $asignar->operario_id }}" required>{{ $asignar->operario->nombreOperario}}</option>
+  @foreach($operario as $operarios)
+    @if($operarios->id != $asignar->operario_id)
+        <option value="{{ $operarios->id }}">{{ $operarios->nombreOperario }}</option>
+    @endif
   @endforeach
   </select>
   </div>
@@ -43,12 +30,8 @@
  <strong style="margin-bot: 10px">Componente:</strong>
  <select name="componente" class="form-control" id="componenteselect">
 
- @if($asignar->componente == null)
- <option value="">No hay componente seleccionada</option>
- @else
+
  <option value="{{ $asignar->componente_id }}" required>{{ $asignar->componente->nombreComponente}}</option>
- @endif
- 
  @foreach($componente as $componente)
 @if($componente->id != $asignar->componente_id)
   <option value="{{ $componente->id }}">{{ $componente->nombreComponente }}</option>
