ecfacfc0bbeaf83d4d802aa06bd49d596bfca02e
Jorge Pizarro <jpt010@alumnos.ucn.cl>
2021-01-22 21:47:43 -0300

    feat(users.table): added ftp user column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             
             $table->id();
             $table->string('name');
+            $table->string('userFTP');
+            $table->string('passFTP');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
